79e5c87f116249d9e0d825b44f98e3d07af67fe3
정원식/게임포털지원개발팀/NE <wonsik.cheung@nhn.com>
2022-02-07 17:34:47 +0900

    feat: pid 생성

--- src/main/kotlin/com/example/demo/spring/image/DemoSpringImageApplication.kt
+++ src/main/kotlin/com/example/demo/spring/image/DemoSpringImageApplication.kt
@@ -1,11 +1,14 @@
 package com.example.demo.spring.image
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.ApplicationPidFileWriter
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class DemoSpringImageApplication
 
 fun main(args: Array<String>) {
-    runApplication<DemoSpringImageApplication>(*args)
+    runApplication<DemoSpringImageApplication>(*args) {
+        addListeners(ApplicationPidFileWriter())
+    }
 }
