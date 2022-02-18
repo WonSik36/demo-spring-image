524ecdfe19e74f3d278dcda988488d0ff410bf43
정원식/게임포털지원개발팀/NE <wonsik.cheung@nhn.com>
2022-02-18 18:55:40 +0900

    init: Batch 브랜치 초기화

---
+++ src/main/kotlin/com/example/demo/spring/image/DemoSpringImageApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo.spring.image
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DemoSpringImageApplication
+
+fun main(args: Array<String>) {
+    runApplication<DemoSpringImageApplication>(*args)
+}
